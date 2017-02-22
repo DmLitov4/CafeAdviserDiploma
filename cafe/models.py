46e5a8d38ccfecdf5839be226f6b690367ae29bb
dmlitov4 <dmitriyonline222@gmail.com>
2017-02-23 00:17:52 +0300

    Front-end changes.

--- cafe/models.py
+++ cafe/models.py
@@ -3,9 +3,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Cities(models.Model):
-	cityname = models.CharField(max_length=30)
-	zipcode = models.IntegerField()
-	created_date = models.DateTimeField(default=timezone.now)
+    cityname = models.CharField(max_length=30)
+    zipcode = models.IntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
 
-def __str__(self):
-	return self.cityname
+    def __str__(self):
+         return self.cityname
